f5a4b9c3b59d8de6946c54b0631386b0677b5660
Wasis Sasoko - AIR <yokowasis@gmail.com>
2023-11-14 15:54:23 +0800

    add config

--- extension.js
+++ extension.js
@@ -1,36 +1,57 @@
+const shelljs = require("shelljs");
+const os = require("os");
+const vscode = require("vscode");
+
+const settingsDIR = `${os.homedir()}/.local/share/code-server/User`;
+const config = vscode.workspace.getConfiguration("sync");
+const gistsID = config.get("GistsID");
+const githubToken = config.get("GithubToken");
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
+async function downloadSettings() {
+  const gistsURL = `https://gist.githubusercontent.com/yokowasis/${gistsID}/raw/settings.json`;
+  const s = await (await fetch(gistsURL)).text();
+  shelljs.ShellString(s).to(`${settingsDIR}/settings.json`);
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "sync" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sync" is now active!');
+  console.log(gistsID);
+  console.log(githubToken);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sync.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  // console.log(shelljs.cat(`${settingsDIR}/settings.json`));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sync!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "sync.helloWorld",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	context.subscriptions.push(disposable);
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from sync!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
